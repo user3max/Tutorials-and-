d2564467466962f370ec9dc560c7746bfc228f53
Carp-Bezverhnii Maxim <maxymyn@gmail.com>
2018-09-25 16:28:05 +0300

    25.09

---
+++ Academy+/Piscine_CPP/Day03/heritage2.cpp
@@ -0,0 +1,54 @@
+#include <string>
+
+class Animal
+{
+private:
+	int _numberOfLegs;
+
+public:
+	Animal();
+	Animal(Animal const &);
+	Animal& operator=(Animal const &);
+	~Animal();
+
+	void run(int distance);
+	void call();
+	void eat(std::string const & what);
+	void walk(int distance);
+};
+
+class Otter : public Animal
+{
+public:
+	Otter();
+	Otter(Otter const &);
+	Otter& operator=(Otter const &);
+	~Otter();
+
+	void scornSomeone(std::string const & target);
+
+};
+
+class Cat : public Animal
+{
+public:
+	Cat();
+	Cat(Cat const &);
+	Cat& operator=(Cat const &);
+	~Cat();
+
+	void scornSomeone(std::string const & target);
+
+};
+
+
+class Pony : public Animal
+{
+public:
+	Pony();
+	Pony(Pony const &);
+	Pony& operator=(Pony const &);
+	~Pony();
+
+	void doMagic(std::string const & target);
+};
